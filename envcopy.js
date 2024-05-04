797226040225f094cc996ee1ec16c34a905c5148
SwapnilAkolkar <click2swapnil@gmail.com>
2024-05-04 10:08:32 +0530

    environment file set up

---
+++ envcopy.js
@@ -0,0 +1,16 @@
+// Important after filling credentials
+// Rename file to env.js 
+var credentials = {
+    'website_app_username': { // Update with actual credentials for each website
+      'username': 'website_app_username',
+      'password': 'website_app_password',
+      'url': "website_url"
+    },
+    'website_app_username': { // Update with actual credentials for each website
+        'username': 'website_app_username',
+        'password': 'website_app_password',
+        'url': "website_url"
+      },
+    // Add more websites and their credentials as needed
+  };
+  var openai_key='OpenAI API KEY';
